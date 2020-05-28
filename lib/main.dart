1e9ed637db1fb7ed168647488b4d5b91f69f659d
Somya-5353 <somyacarmel5353@gmail.com>
2020-05-28 23:09:43 +0530

    populating the task list using bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:weeklyplanner/Widgets/LandingScreen.dart';
-import './Widgets/user_input.dart';
-import 'Model/TaskModel.dart';
-import 'Widgets/make_user_input_sheet.dart';
 
 void main() => runApp(MyHome());
 
