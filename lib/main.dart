5647760fcaf6c802dc657e7f51d9cd86ab5b7ccf
Somya-5353 <somyacarmel5353@gmail.com>
2020-05-28 21:41:16 +0530

    home screen logic widget implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weeklyplanner/Widgets/LandingScreen.dart';
 import './Widgets/user_input.dart';
 import 'Model/TaskModel.dart';
 import 'Widgets/make_user_input_sheet.dart';
@@ -16,48 +17,11 @@ class MyHome extends StatelessWidget {
 }
 
 class MyApp extends StatelessWidget {
-  List<TaskModel> tasklist = [];
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Weekly Planner'),
-          backgroundColor: Colors.redAccent,
-        ),
-        body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 300,
-                padding: EdgeInsets.all(20),
-                child: Card(
-                  color: Colors.teal,
-                  child: Text("Card"),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: UserInput(),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) => Container(
-                child: MakeUserInputSheet(),
-              ),
-            );
-          },
-        ),
-      ),
+      home: LandingScreen(),
     );
   }
 }
